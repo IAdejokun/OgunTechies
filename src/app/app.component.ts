9ae0d1937ea990f15e349676a42021264d514a47
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2024-11-10 16:22:58 +0100

    initial route, styling, config made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { RouterOutlet, Event, NavigationEnd, Router } from '@angular/router';
+import { IStaticMethods } from 'preline/preline';
+import { Subscription } from 'rxjs';
+
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -8,6 +16,27 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private routerSubscription!: Subscription;  // Added ! here
   title = 'oguntechies';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.routerSubscription = this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          if (window.HSStaticMethods) {
+            window.HSStaticMethods.autoInit();
+          }
+        }, 100);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
 }
