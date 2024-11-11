0d8f0cfd97ab2e62643d52a6c659423e57da0d5f
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2024-11-11 18:35:40 +0100

    home component updated and modularised

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BenefitsCardComponent } from '../../components/home/benefits-card/benefits-card.component';
+import { CommunityProjectsCardComponent } from '../../components/home/community-projects-card/community-projects-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [BenefitsCardComponent, CommunityProjectsCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
