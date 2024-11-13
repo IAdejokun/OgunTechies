e4a06a220f084ea5c03e4c0c823c62c9225feb9c
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2024-11-13 14:05:07 +0100

    components initialised and updated

---
+++ src/app/components/home/faq-accordion/faq-accordion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-accordion',
+  standalone: true,
+  imports: [],
+  templateUrl: './faq-accordion.component.html',
+  styleUrl: './faq-accordion.component.css'
+})
+export class FaqAccordionComponent {
+
+}
